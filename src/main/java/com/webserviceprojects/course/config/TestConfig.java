36763e3a03b29374f45c0e2e055cb2f70043a925
Julio Cesar <kucip2234@gmail.com>
2023-11-30 13:03:48 -0300

    OrderStatus enum

--- src/main/java/com/webserviceprojects/course/config/TestConfig.java
+++ src/main/java/com/webserviceprojects/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.webserviceprojects.course.entities.Order;
 import com.webserviceprojects.course.entities.User;
+import com.webserviceprojects.course.entities.enums.OrderStatus;
 import com.webserviceprojects.course.repositories.OrderRepository;
 import com.webserviceprojects.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Patricia", "patricia@email.com", "9632517", "1234567");
 		User u2 = new User(null, "Geraldo", "geraldo@email.com", "2478682", "7654321");
 				
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
