8989fb3899f0e5f386471b1a21102ba047603cec
Julio Cesar <kucip2234@gmail.com>
2023-12-04 13:26:33 -0300

    Product

---
+++ src/main/java/com/webserviceprojects/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.webserviceprojects.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webserviceprojects.course.entities.Product;
+import com.webserviceprojects.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		  Optional<Product> obj = repository.findById(id);
+		  return obj.get();
+	}
+}
