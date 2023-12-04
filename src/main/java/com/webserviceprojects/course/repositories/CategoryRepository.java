796cca767a48a9e4ef98a68861ed7b7a9be7b6b0
Julio Cesar <kucip2234@gmail.com>
2023-12-04 12:52:58 -0300

    Category

---
+++ src/main/java/com/webserviceprojects/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.webserviceprojects.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webserviceprojects.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
