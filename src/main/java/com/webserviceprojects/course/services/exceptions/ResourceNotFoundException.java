275b003f1953bc2049df20c5bcd2e1ccf5ae65f1
Julio Cesar <kucip2234@gmail.com>
2023-12-11 11:56:06 -0300

    Exception handling - findById

---
+++ src/main/java/com/webserviceprojects/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.webserviceprojects.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
